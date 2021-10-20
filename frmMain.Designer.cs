b7e07211a4d6af6b89403f3bb4f07f6871e6e8ff
Alan Barber <alan.barber@improving.com>
2021-10-20 07:25:47 -0400

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace SlideShow
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+            this.pictureBox.Location = new System.Drawing.Point(137, 80);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(846, 507);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.DoubleClick += new System.EventHandler(this.pictureBox_DoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 60000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1111, 707);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "frmMain";
+            this.Text = "SlideShow";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyUp);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SlideShow
+{
+    public partial class frmMain : Form
+    {
+        private bool formFullscreen = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            loadRandomPicture();
+            timer.Enabled = true;
+            timer.Interval = 60000;
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            pictureBox.Top = 0;
+            pictureBox.Left = 0;
+            pictureBox.Width = this.Width;
+            pictureBox.Height = this.Height;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            loadRandomPicture();
+        }
+
+        private void loadRandomPicture()
+        {
+            var extensions = new string[] { ".png", ".jpg", ".gif" };
+            var di = new DirectoryInfo(System.IO.Directory.GetCurrentDirectory());
+            var rgFiles = di.GetFiles("*.*").Where(f => extensions.Contains(f.Extension.ToLower())).ToList();
+            if (rgFiles.Any())
+            {
+                Random R = new Random();
+                var file = rgFiles.ElementAt(R.Next(0, rgFiles.Count())).FullName;
+                pictureBox.Load(file);
+            }
+        }
+
+        private void pictureBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (!formFullscreen)
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Maximized;
+            }
+
+            this.formFullscreen = !this.formFullscreen;
+        }
+
+        private void frmMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    this.Close();
+                    break;
+                case Keys.Space:
+                    timer.Enabled = !timer.Enabled;
+                    this.Text = timer.Enabled ? "SlideShow" : "SlideShow - Paused";
+                    break;
+                case Keys.Right:
+                    loadRandomPicture();
+                    this.timer.Stop();
+                    this.timer.Start();
+                    break;
+            }
+        }
+    }
+}
